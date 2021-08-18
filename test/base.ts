9b8cc1c4fa73d9b43a7a084336eb995aaab6ae04
Alvaro Assmus Nassar <alvaroassmus@gmail.com>
2021-08-18 16:09:47 -0500

    test script added with mocha params initial base test file created to use it to create more tests if need it.

---
+++ test/base.ts
@@ -0,0 +1,10 @@
+let expect = require("chai").expect;
+
+describe("Your test text", function () {
+    describe("to a specific functionality", function () {
+        it("makes the test for it", function () {
+            let theValue = 1;
+            expect(theValue).to.equal(1);
+        });
+    });
+});
