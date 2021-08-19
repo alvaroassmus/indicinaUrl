4dbd4a4b49f57a8d1787877103dd315918225f04
Alvaro Assmus Nassar <alvaroassmus@gmail.com>
2021-08-18 19:23:22 -0500

    UPDATE: encode ENDPOINT added to pass the Encode tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 let express = require("express");
 let app = express();
 
+app.get("/encode/*", function (req, res) {
+    const url = { urlEncoded : "urlEncoded", msg : "Url has been encoded" };
+    res.json(url);
+});
+
 app.listen(3000);
